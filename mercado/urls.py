2b8aa227ef5e8ae2708926dd82b5703a0cf753db
LCSRuas <lucasiruas@gmail.com>
2024-02-18 18:30:40 -0300

    chore: remocao da pagina inicial

--- mercado/urls.py
+++ mercado/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import pag_inicial, produto
+from .views import produto
 from django.urls import include
 
 urlpatterns = [
-    path('', pag_inicial, name='pag_inicial'),
     path('produto', produto, name='produto')
-]   
+]
--- mercado/views.py
+++ mercado/views.py
@@ -5,8 +5,5 @@ from django.contrib.auth import authenticate
 from django.contrib.auth import login as login_django
 from django.contrib.auth import logout as logout_django
 
-def pag_inicial(request):
-    return render(request, "pag_inicial.html")
-
 def produto(request):
     return render(request, "./produto.html")
